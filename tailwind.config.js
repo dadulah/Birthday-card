d1e6f053d1ecdf50ff2ce82942ac86c235ee43d6
rawdydev <dadullahakbary@gmail.com>
2025-01-25 01:11:40 +0330

    added kamona to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html,js}"],
+  mode: 'jit',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@kamona/tailwindcss-perspective'),
+  ],
 }
 
