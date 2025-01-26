af21de649b84109205c6f5d4f12c439f44b76f27
rawdydev <dadullahakbary@gmail.com>
2025-01-27 02:47:16 +0330

    learend and created the original version of the card with the help of v0

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["*.{html,js}"],
   mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'cake-layer': `linear-gradient(transparent 50px, #fedbab 50px, #fedbab 60px, transparent 60px),
+        radial-gradient(circle at 30px 5px, #994c10 30px, #fcbf29 31px)`,
+        'candle': `repeating-linear-gradient(45deg, #fd3018 0, #fd3018 5px, #ffa89e 5px, #ffa89e 10px)`
+      }
+    },
   },
   plugins: [
     require('@kamona/tailwindcss-perspective'),
