cb03e931e662562a329e7b9a407558fb04ac9a33
rawdydev <dadullahakbary@gmail.com>
2025-01-28 01:33:01 +0330

    chaged some colors and sizings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'cake-layer': `linear-gradient(transparent 50px, #fedbab 50px, #fedbab 60px, transparent 60px),
-        radial-gradient(circle at 30px 5px, #994c10 30px, #fcbf29 31px)`,
-        'candle': `repeating-linear-gradient(45deg, #fd3018 0, #fd3018 5px, #ffa89e 5px, #ffa89e 10px)`
+        'cake-layer': `linear-gradient(transparent 50px, #BCECF8 50px, #BCECF8 60px, transparent 60px),
+        radial-gradient(circle at 30px 5px, #26C8B9 30px, #8FE7FD 31px)`,
+        'candle': `repeating-linear-gradient(45deg, #72B6D8 0, #72B6D8 5px, #D5F0F7 5px, #D5F0F7 10px)`
       }
     },
   },
