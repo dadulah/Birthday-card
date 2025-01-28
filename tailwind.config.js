87569857a658d5171f1af0bceed669384dc20db6
rawdydev <dadullahakbary@gmail.com>
2025-01-29 00:21:09 +0330

    added some fixation to the card utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html,js}"],
-  mode: 'jit',
   theme: {
     extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
       backgroundImage: {
         'cake-layer': `linear-gradient(transparent 50px, #BCECF8 50px, #BCECF8 60px, transparent 60px),
         radial-gradient(circle at 30px 5px, #26C8B9 30px, #8FE7FD 31px)`,
         'candle': `repeating-linear-gradient(45deg, #72B6D8 0, #72B6D8 5px, #D5F0F7 5px, #D5F0F7 10px)`
-      }
+      },
     },
   },
-  plugins: [
-    require('@kamona/tailwindcss-perspective'),
-  ],
+  plugins: [],
 }
 
